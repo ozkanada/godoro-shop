6a4302018186d155fee00024c13bc307eee448e4
gogo beko <gogobeko@gmail.com>
2013-12-25 17:56:40 +0200

    productPrice field i eklendi

--- src/java/entity/Product.java
+++ src/java/entity/Product.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package entity;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,18 +19,29 @@ import javax.persistence.Table;
  * @author GOGO
  */
 @Entity
-@Table(catalog="javashopdb")
-public class Product implements Serializable{
-    @Id 
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="ProductId")
+@Table(catalog = "javashopdb")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ProductId")
     private long productId;
-    @Column(name="ProductName")
+    @Column(name = "ProductName")
     private String productName;
+    @Column(name = "ProductPrice")
+    private double productPrice;
 
     public Product() {
     }
-    
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+ 
     public long getProductId() {
         return productId;
     }
@@ -46,6 +57,5 @@ public class Product implements Serializable{
     public void setProductName(String productName) {
         this.productName = productName;
     }
-    
-    
+
 }
